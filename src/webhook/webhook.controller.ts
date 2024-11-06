1de747560f990eb0ed2cc966af5159b9673807ba
Pedro Serrano <pedro.serrano@makes.ai>
2024-11-05 21:28:52 -0300

    feat: criados module e controller webhook

---
+++ src/webhook/webhook.controller.ts
@@ -0,0 +1,5 @@
+import { Controller, Get, Query, Res } from '@nestjs/common';
+import { Response } from 'express';
+import * as dotenv from 'dotenv';
+@Controller('webhook')
+export class WebhookController {}
